36e52e8f969434f188d54386e423452f75885944
Mayra A. Rodríguez M <mayrarodriguez1709@gmail.com>
2016-12-02 11:52:34 -0500

    Fix and adaptation

--- src/pages/entry-modal/entry-modal.ts
+++ src/pages/entry-modal/entry-modal.ts
@@ -10,18 +10,74 @@ export class EntryModalPage {
   entry: any;
   entryCategories: any;
 
+  customForm: any[] = [];
+
   constructor(
     public navCtrl: NavController,
     public viewCtrl: ViewController,
     public params: NavParams) {
     
     this.entryCategories = params.get('entryCategories');
+    console.log(this.entryCategories)
+
     this.entry = {
       idCategory: 1,
       amount : 0,
       date : null,
       description: ""
     }
+
+    this.customForm = [
+      {
+        name: 'categories',
+        type: 'select',
+        label: 'Categorías',
+        options: [
+          {
+            "value":1,
+            "label": "Salario",
+          },
+          {
+            "value":2,
+            "label": "Honorarios"
+          },
+          {
+            "value":3,
+            "label": "Extras"
+          },
+          {
+            "value":4,
+            "label": "Regalos"
+          },
+          {
+            "value":5,
+            "label": "Prestamos o adelantos"
+          },
+          {
+            "value":6,
+            "label": "Otros ingresos"
+          }
+        ]
+      },
+      {
+        name: 'amount',
+        type: 'text',
+        value: '',
+        label: 'Monto'
+      },
+      {
+        name: 'date',
+        type: 'date',
+        value: '',
+        label: 'Fecha'
+      },
+      {
+        name: 'description',
+        type: 'text',
+        value: '',
+        label: 'Descripción'
+      }
+    ]
   }
 
   ionViewDidLoad() {
