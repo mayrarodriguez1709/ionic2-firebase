5208bae643ac961adac07964bcc04ac5c6041204
Mayra A. Rodríguez M <mayrarodriguez1709@gmail.com>
2016-12-02 11:09:54 -0500

    Fix in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,23 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MenuPage } from '../pages/menu/menu';
+import { DashboardPage } from '../pages/dashboard/dashboard';
+import { EntryCategoriesPage } from '../pages/entry-categories/entry-categories';
+import { ExitCategoriesPage } from '../pages/exit-categories/exit-categories';
+import { EntryModalPage } from '../pages/entry-modal/entry-modal';
+import { ExitModalPage } from '../pages/exit-modal/exit-modal';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    MenuPage,
+    DashboardPage,
+    EntryCategoriesPage,
+    ExitCategoriesPage,
+    EntryModalPage,
+    ExitModalPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +26,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    MenuPage,
+    DashboardPage,
+    EntryCategoriesPage,
+    ExitCategoriesPage,
+    EntryModalPage,
+    ExitModalPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
